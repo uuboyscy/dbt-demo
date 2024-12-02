3daf9c86c6b2f6373f921778f3c5a67c8c422cf6
uuboyscy <aegis12321@gmail.com>
2024-12-02 09:53:46 +0800

    feat: Startup script

--- run-poc-postgresql.sh
+++ run-poc-postgresql.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+echo "Building Postgres container and starting it."
 docker run \
     -d \
     --name dbt-postgres \
@@ -8,6 +11,10 @@ docker run \
     -e POSTGRES_DB=dbt \
     postgres:latest
 
+echo "Copying create-tables.sql to dbt-postgres container"
 docker cp create-tables.sql dbt-postgres:/create-tables.sql
 
+sleep 3s
+
+echo "Executing SQL script in dbt-postgres container"
 docker exec -i dbt-postgres psql -U postgres -d dbt -f /create-tables.sql
